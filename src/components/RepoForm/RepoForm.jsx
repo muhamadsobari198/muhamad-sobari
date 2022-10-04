37bdc6d7aa4ebbe8966b63a5f250539122b9ed70
ms198 <muhamadsobari198@gmail.com>
2022-10-04 10:28:09 +0700

    logic update

--- src/components/RepoForm/RepoForm.jsx
+++ src/components/RepoForm/RepoForm.jsx
@@ -1,109 +1,69 @@
 import './repoform.css';
-import {
-	AccountCircleOutlined,
-	CloseOutlined,
-	EmailOutlined,
-	LinkOutlined,
-	LocationOnOutlined
-} from '@material-ui/icons';
+import { AccountCircleOutlined } from '@material-ui/icons';
 import { useState } from 'react';
 import axios from 'axios';
 
+import BackdropRepo from './../Backdrop/BackdropRepo';
+import BackdropError from './../Backdrop/BackdropError';
+
 const RepoForm = () => {
+	const [ isFetching, setIsFetching ] = useState(false);
 	const [ isOpen, setIsOpen ] = useState('out');
-	const [ username, setUsername ] = useState('muhamadsobari198');
+	const [ isError, setIsError ] = useState('not');
+	const [ errMessage, setErrMessage ] = useState('');
+	const [ username, setUsername ] = useState();
 	const [ user, setUser ] = useState({});
 	const [ repos, setRepos ] = useState([]);
 
 	const handleClick = async (e) => {
 		e.preventDefault();
 
-		const user = await axios.get(`${API_GITHUB}/${username}`);
-		const repos = await axios.get(`${API_GITHUB}/${username}/repos`);
-		console.log(repos);
-		setUser(user.data);
-		setRepos(repos.data);
-		isModal('in');
+		setIsFetching(true);
+
+		try {
+			const user = await axios.get(`${API_GITHUB}/${username}`);
+			const repos = await axios.get(`${API_GITHUB}/${username}/repos`);
+
+			setIsFetching(false);
+			setUser(user.data);
+			setRepos(repos.data);
+			isModal('in');
+		} catch (e) {
+			setIsFetching(false);
+
+			const message = e.response && e.response.data ? e.response.data.message : e.message;
+
+			setErrMessage(message);
+			isModalError('yes');
+		}
 	};
 
 	const isModal = (v) => {
 		setIsOpen(v);
 	};
 
+	const isModalError = (v) => {
+		setIsError(v);
+	};
+
 	return (
-		<div>
-			<div className="backdrop" isopen={isOpen}>
-				<div className="container">
-					<div className="wrapperRepo">
-						<div className="top">
-							<CloseOutlined className="icon" onClick={() => isModal('out')} />
-						</div>
-						<div className="bottom">
-							<div className="account">
-								<div className="profile">
-									<img src={user.avatar_url} />
-									<h3>{user.name}</h3>
-									<h5>{user.login}</h5>
-									<span className="bio">{user.bio}</span>
-								</div>
-								<hr />
-								<div className="social">
-									<span className="location">
-										<LocationOnOutlined className="icon" />
-										{user.location}
-									</span>
-									<span className="email">
-										<EmailOutlined className="icon" />
-										{user.email ? user.email : '-'}
-									</span>
-									<span className="blog">
-										<LinkOutlined className="icon" />
-										{user.blog}
-									</span>
-								</div>
-							</div>
-							<div className="repositories">
-								<h4 className="topTitle"> List repositories : </h4>
-								<div className="bottom">
-									{repos.map((item, i) => {
-										return (
-											<div className="cardRepo" key={i}>
-												<div>
-													<a href={item.html_url} target="_blank">
-														<h6>
-															{++i}. {item.name}
-														</h6>
-													</a>
-													<span className="description">{item.description?.substr(0, 30) } {(item.description?.length >30 ) && '...'}</span>
-												</div>
-												<div>
-													<span className="lang">{item.language}</span>
-												</div>
-											</div>
-										);
-									})}
-								</div>
-							</div>
-						</div>
-					</div>
+		<div className="repoForm">
+			<div className="form">
+				<div className="search">
+					<AccountCircleOutlined className="icon" />
+					<input
+						type="text"
+						placeholder="Type username here..."
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button onClick={(e) => handleClick(e)} disabled={!username || isFetching}>
+						Search
+					</button>
 				</div>
 			</div>
 
-			<div className="repoForm">
-				<div className="form">
-					<div className="search">
-						<AccountCircleOutlined className="icon" />
-						<input
-							type="text"
-							placeholder="Type username here..."
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-						<button onClick={(e) => handleClick(e)} disabled={!username}>
-							Search
-						</button>
-					</div>
-				</div>
-			</div>
+			<BackdropRepo isOpen={isOpen} user={user} repos={repos} errMessage={errMessage} isModal={isModal} />
+			<BackdropError isError={isError} errMessage={errMessage} isModalError={isModalError} />
 		</div>
 	);
 };
